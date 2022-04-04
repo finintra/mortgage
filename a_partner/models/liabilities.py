976630506a223c6a2aaa9036cff95f0dc6f9b956
finintra <a.finberg@itransition.com>
2022-04-04 11:22:45 +0300

    new Module

---
+++ a_partner/models/liabilities.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+
+class Liabilities(models.Model):
+    _name = "liabilities"
+
+    name = fields.Char(required=True)
+    owner_applicant = fields.Char()
+    type_applicant = fields.Char()
+    lender_applicant = fields.Char()
+    balance_applicant = fields.Char()
+    monthly_applicant = fields.Char()
+    end_date_applicant = fields.Date()
+    mortgage_applicant = fields.Char()
+    costs_associated = fields.Boolean()
+    possible_consequences = fields.Boolean()
+    judgments = fields.Boolean()
+    settled = fields.Boolean()
+    bankrupt = fields.Boolean()
+    discharged = fields.Boolean()
+    associated_notes = fields.Text()
